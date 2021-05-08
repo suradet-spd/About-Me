465706f20e0ce136b7425ac61bbd29f241bfa4d7
yubanarchy <yubanarchy@gmail.com>
2021-05-08 15:07:57 +0700

    [edit] register validate lang

--- resources/lang/en/register.php
+++ resources/lang/en/register.php
@@ -18,4 +18,31 @@ return [
     "AlreadyEmail" => "Email already exists",
     "RegisterResultComplete" => "Register complete",
     "RegisterResultError" => "Can't Register ! Please try again.",
+
+    // Validate Register form [error message]
+    // [Image]
+        "err_image_mimes" => "Only image [jpeg , jpg , png] files.",
+        "err_image_size" => "Supports a maximum file size of 10 MB.",
+        "err_image_req" => "Please enter your profile picture.",
+    // [Nickname]
+        "err_nick_req" => "Please enter your nickname.",
+        "err_nick_max" => "Your nickname is too long.",
+        "err_nick_str" => "Only characters are allowed.",
+    // [Name thai]
+        "err_name_th_req" => "Please enter your name [thai].",
+        "err_name_th_max" => "Your name [thai] is too long.",
+        "err_name_th_str" => "Only characters are allowed.",
+    // [Name eng]
+        "err_name_en_req" => "Please enter your name [eng].",
+        "err_name_en_max" => "Your name [eng] is too long.",
+        "err_name_en_str" => "Only characters are allowed.",
+    // [email]
+        "err_email_req" => "Please enter your email.",
+        "err_email_max" => "Your email is too long.",
+        "err_email_mail" => "Your Email is invalid.",
+        "err_email_str" => "Only characters are allowed.",
+    // [email]
+        "err_pass_req" => "Please enter password.",
+        "err_pass_min" => "The minimum password must be 8 characters or more.",
+        "err_pass_confirm" => "Your confirm password does not match.",
 ];
--- resources/lang/th/register.php
+++ resources/lang/th/register.php
@@ -18,4 +18,31 @@ return [
     "AlreadyEmail" => "บัญชีอีเมล์นี้ถูกใช้งานแล้ว",
     "RegisterResultComplete" => "สมัครสมาชิกเรียบร้อยแล้ว",
     "RegisterResultError" => "ไม่สามารถสมัครสมาชิกได้ ! โปรดลองใหม่อีกครั้ง.",
+
+    // Validate Register form [error message]
+    // [Image]
+        "err_image_mimes" => "เฉพาะไฟล์ jpeg , jpg , png เท่านั้น",
+        "err_image_size" => "รองรับขนาดไฟล์สูงสุด 10 MB.",
+        "err_image_req" => "โปรดอัพโหลดรูปโปรไฟล์",
+    // [Nickname]
+        "err_nick_req" => "โปรดระบุชื่อเล่นของคุณ",
+        "err_nick_max" => "ชื่อเล่นของคุณยาวเกินไป",
+        "err_nick_str" => "อนุญาตเฉพาะอักษรเท่านั้น",
+    // [Name thai]
+        "err_name_th_req" => "โปรดระบุชื่อ (ไทย) ของคุณ",
+        "err_name_th_max" => "ชื่อ (ไทย) ของคุณยาวเกินไป",
+        "err_name_th_str" => "อนุญาตเฉพาะอักษรเท่านั้น",
+    // [Name eng]
+        "err_name_en_req" => "โปรดระบุชื่อ (อังกฤษ) ของคุณ",
+        "err_name_en_max" => "ชื่อ (อังกฤษ) ของคุณยาวเกินไป",
+        "err_name_en_str" => "อนุญาตเฉพาะอักษรเท่านั้น",
+    // [email]
+        "err_email_req" => "โปรดระบุอีเมล์ของคุณ",
+        "err_email_max" => "อีเมล์ของคุณยาวเกินไป",
+        "err_email_mail" => "โปรดระบุอีเมล์ที่ถูกต้อง",
+        "err_email_str" => "อนุญาตเฉพาะอักษรเท่านั้น",
+    // [password]
+        "err_pass_req" => "โปรดระบุรหัสผ่านของคุณ",
+        "err_pass_min" => "รหัสผ่านขั้นต่ำ 8 ตัวอักษรขึ้นไป",
+        "err_pass_confirm" => "ยืนยันรหัสผ่านไม่ถูกต้อง",
 ];
