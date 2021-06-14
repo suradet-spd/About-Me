31994deaf5a8fa074ba7db111d9e726c79164d8b
yubanarchy <yubanarchy@gmail.com>
2021-06-14 21:26:02 +0700

    [edit] alert login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -279,9 +279,9 @@
                         <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                         @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                            <script>
+                                swal("{{ trans('home.Error_msg_header') }}" , "{{ $message }}" , 'error');
+                            </script>
                         @enderror
                     </div>
                 </div>
@@ -293,9 +293,9 @@
                         <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                         @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
+                            <script>
+                                swal("{{ trans('home.Error_msg_header') }}" , "{{ $message }}" , 'error');
+                            </script>
                         @enderror
                     </div>
                 </div>
